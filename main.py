d7e60d94216aad5764b58d9c4551faf5cba3e948
brainspill3r <hacker01bug@gmail.com>
2025-03-03 15:00:51 +0000

    finished bookbot

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-from stats import get_num_words
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-from stats import counting_chars
-from stats import get_book_text
 
 def main():
-    path = "books/frankenstein.txt"
-    text = get_book_text(path)
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-    word_count = get_num_words(path)
-    print(f"{word_count} words found in the document")
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-    char_counts = counting_chars(text)
 
-    # Print the character counts
-    print(char_counts)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
